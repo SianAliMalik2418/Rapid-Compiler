056dcf985a80f21c6ab91a0e96f7e48b25f794fe
SianAliMalik2418 <sianmalik2418@gmail.com>
2024-02-20 07:54:49 +0500

    bugs fixed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,6 @@ import { FaLocationArrow } from "react-icons/fa6";
 import CodeArea from './CodeArea'
 import { dataContext } from './Context';
 import { Link } from 'react-router-dom';
-import Footer from './Footer';
 
 function HeroSection() {
 
@@ -19,14 +18,14 @@ function HeroSection() {
 
     <div className='relative  w-full flex flex-col pt-5 items-center text-white md:mt-8 '>
 
-      <h1 className='text-3xl font-medium  text-center '>Effortless Coding, Seamless Compiling</h1>
+      <h1 className='text-3xl font-medium  text-center'>Effortless Coding, Seamless Compiling</h1>
 
       <div className='mt-4 font-light flex flex-col items-center '>
         <p>Welcome to Rapid Compiler - Your</p>
         <p>Ultimate Code Compiler.</p>
       </div>
 
-      <Link to={"/language"} className='border-2  pb-4 pt-2 mt-10 flex items-center justify-center px-10 gap-5 ' onClick={() => setShowLanguageSection(!showLanguageSection)}>
+      <Link to={"/language"} className='border-2  pb-4 pt-2 mt-10 flex items-center justify-center px-10 gap-5 ' >
         <div className='flex items-center gap-2 relative'>
           <h1>Language</h1>
           <p className='text-xs absolute top-6 left-12 text-[yellow]'>{`{${language}}`}</p>
